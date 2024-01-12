61aca36952ced6437f000f5be3d16e6ebafd2dd8
chana-rozin <c0556787069@gmail.com>
2024-01-12 02:24:00 +0200

    continue in login

--- APP/src/App.jsx
+++ APP/src/App.jsx
@@ -1,11 +1,42 @@
 import './App.css'
 import LogIn from './Components/LogIn/LogIn'
+import Home from './Components/Home/Home'
+import { React, createContext, useState } from 'react'
+//import { ReactDOM } from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate, Link} from 'react-router-dom'
+
+export const userContext = createContext()
+
 function App() {
 
+  const [currentUser, setCurrentUser] = useState(null)
+  fetch("http://localhost:3000/users?username=Bret&website=hildegard.org").then(re=>re.json()).then(r=>console.log(r))
 
   return (
     <>
-      <LogIn />
+      <userContext.Provider value={{ currentUser, setCurrentUser }}>
+        <Router>
+          <Routes>
+          currentUser!=null ? <Navigate to="/home" /> : <Navigate to="/login" />
+            {/* <Route path="/" element={<Home/>}></Route> */}
+            <Route path="/login" element={<LogIn />}></Route>
+             <Route path="/home" element={<Home/>}></Route>
+          {/*<Route path="/login" element={<LogIn/>}></Route>
+          <Route path="/login" element={<LogIn/>}></Route>
+          <Route path="/login" element={<LogIn/>}></Route>
+          <Route path="/login" element={<LogIn/>}></Route>
+          <Route path="/login" element={<LogIn/>}></Route> */}
+            <Route
+              path="/"
+              element={
+                <div>
+                  <h2>404 Page not found</h2>
+                </div>
+              }
+            />
+          </Routes>
+        </Router>
+      </userContext.Provider>
     </>
   )
 }
--- APP/src/Components/LogIn/LogIn.jsx
+++ APP/src/Components/LogIn/LogIn.jsx
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { useCallback, useContext, useRef } from "react";
 import { useState } from "react";
+import { userContext } from "../../App";
 
 
 const LogIn=()=>{
 
-   const[userName,setUserName] = useState("");
-   const[website,setWebsite] = useState("");
-
+   const userNameRef = useRef("");
+   const websiteRef = useRef("");
+   const errMessageRef = useRef("");
+   const {currentUser, setCurrentUser} = useContext(userContext);
    function handleSubmit(event)
    {
-     
+     event.preventDefault();
+     fetch(`http://localhost:3000/users?username=${userNameRef.current.value}&website=${websiteRef.current.value}`)
+     .then(result=>result.json())
+     .then(json=>
+        {
+            console.log(json);
+            navToHomePage(json)})
+     .catch(error=>
+        errMessageRef.current="Incorrect username or password")
    }
+
+   function navToHomePage(userDetails){
+    localStorage.setItem("currentUser", userDetails)
+    setCurrentUser(userDetails);
+   }
+
     
     return(
         <>
             <div className="login-wrapper">
                 <h1>Please Log In</h1>
-                <form>
+                <form onSubmit={handleSubmit}> 
                 <label>
                     <p>Username</p>
-                    <input type="text" />
+                    <input ref={userNameRef} type="text" />
                 </label>
                 <label>
                     <p>Website</p>
-                    <input type="password" />
+                    <input ref={websiteRef} type="password" />
                 </label>
                 <div>
                     <button type="submit">Submit</button>
                 </div>
                 </form>
             </div>
+            <p>{errMessageRef.current.value}</p>
         </>
     )
 }
