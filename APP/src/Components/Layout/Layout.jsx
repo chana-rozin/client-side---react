bc33e5cd3d8d89ffe1cb1ed31464318021caa489
chana-rozin <c0556787069@gmail.com>
2024-01-22 21:44:41 +0200

    navBar style

--- APP/src/Components/Layout/Layout.jsx
+++ APP/src/Components/Layout/Layout.jsx
@@ -1,43 +1,42 @@
-import { Link , Outlet, useNavigate, useParams} from "react-router-dom"
-import React, {useContext} from "react";
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom"
+import React, { useContext } from "react";
 import { userContext } from "../../App";
 import style from "./Layout.module.css";
 
-const Layout = ()=>{
+const Layout = () => {
 
     const navigate = useNavigate();
-    const {currentUser, setCurrentUser} = useContext(userContext);
-    const userId= currentUser.id;
-    function handleLogout(){
+    const { currentUser, setCurrentUser } = useContext(userContext);
+    const userId = currentUser.id;
+    function handleLogout() {
         localStorage.removeItem("currentUser");
         navigate("/login");
         setCurrentUser(null);
     }
 
-    
+
     return (
-        <>
+        <><header>
             <h1>{currentUser.name}</h1>
-            <div className={style.links}>
-            <nav className={style.c}>
-                    <Link to={`/users/${userId}/info`} >
-                        <dt>Info</dt>
-                    </Link>
-                    <Link to={`/users/${userId}/todos`} >
-                        <dt>Todos</dt>
-                    </Link>
-                    <Link to={`/users/${userId}/posts`} >
-                        <dt>Posts</dt>
-                    </Link>
-                    <Link to={`/users/${userId}/albums`} >
-                        <dt>Albums</dt>
-                    </Link>
-                    <a onClick={()=>handleLogout()} >
-                        <dt>Logout</dt>
-                   </a>
+            <nav className={style.links}>
+                <Link to={`/users/${userId}/info`} >
+                    <dt>Info</dt>
+                </Link>
+                <Link to={`/users/${userId}/todos`} >
+                    <dt>Todos</dt>
+                </Link>
+                <Link to={`/users/${userId}/posts`} >
+                    <dt>Posts</dt>
+                </Link>
+                <Link to={`/users/${userId}/albums`} >
+                    <dt>Albums</dt>
+                </Link>
+                <a onClick={() => handleLogout()} >
+                    <dt>Logout</dt>
+                </a>
             </nav>
-            </div>
-            <Outlet/>
+        </header>
+            <div className={style.content}><Outlet /></div>
         </>
     )
 }
